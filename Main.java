ede81da9a0aaa933c7507b3adfbaf2e7ddafe6f4
Ansere <68030201+Ansere@users.noreply.github.com>
2021-06-27 21:07:12 -0500

    added getMissingDates function

--- Main.java
+++ Main.java
@@ -1,145 +1,217 @@
-import com.hamoid.VideoExport;
-import processing.core.PFont;
 import processing.core.PApplet;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Date;
+import java.io.PrintWriter;
+import java.sql.Array;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class Main extends PApplet{
+public abstract class DataReader {
 
-    String[] monthNames = {"JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"};
+    private static ArrayList<ArrayList<String>> modifiedPerson2D;
 
-    int frames = 0;
-    int PEOPLE_COUNT;
-    private int DAY_LEN;
-    int TOP_VISIBLE = 15;
-    float FRAMES_PER_DAY = (float) 15;
-    int START_DAY = 0;
-    int START_DATE = dateToDays("2020-09-09");
-    int ROCKS_START_DATE = dateToDays("2021-03-29");
-    BarGraph[] corners = new BarGraph[4];
-
-    public PFont font;
-    public PFont numberFont;
-
-    final int NORTHWEST = 0;
-    final int NORTHEAST = 2;
-    final int SOUTHEAST = 3;
-    final int SOUTHWEST = 1;
-    private VideoExport videoExport;
-
-    public static void main(String[] args){
-        PApplet.main("Main", args);
+    public static void main(String[] args) throws FileNotFoundException {
+        makeCSV();
     }
 
-    public void setup(){
-        try {
-            DataReader.makeCSV();
+    public static void makeCSV() throws FileNotFoundException {
+        ArrayList<ArrayList<String>> matrix = new ArrayList<>();
+        String[] csv = PApplet.loadStrings(new File("data\\orni.csv"));
+        removeCommas(csv);
+        for (int i = 0; i < csv.length; i++) {
+            String[] person = csv[i].split(",");
+            if (person[1].equals("0")){
+                continue;
+            }
+            ArrayList<String> resultPerson = new ArrayList<>();
+            for (int d = 0; d < person.length; d++) {
+                if (d == 1) {
+                    resultPerson.add("Orni");
+                } else if (d > 2 && i > 0) {
+                    resultPerson.add(Integer.parseInt(resultPerson.get(d - 1)) + Integer.parseInt(person[d]) + "");
+                } else {
+                    resultPerson.add(person[d]);
+                }
+            }
+            matrix.add(resultPerson);
+        }
+        csv = PApplet.loadStrings(new File("data\\fossils.csv"));
+        removeCommas(csv);
+        String[] fossilDates = Arrays.stream(csv[0].split(",")).skip(2).collect(Collectors.toList()).toArray(new String[0]);
+        String[] missingFossilDates = getMissingDates(fossilDates[0], LocalDate.now().plusDays(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")), fossilDates);
+        modifiedPerson2D = Arrays.stream(csv).map(s -> new ArrayList<>(Arrays.asList(s.split(",")))).collect(Collectors.toCollection(ArrayList::new));
+        modifiedPerson2D = transposeMatrix(modifiedPerson2D);
+        for (int d = 0; d < missingFossilDates.length; d++){
+            ArrayList<String> date = IntStream.range(0, modifiedPerson2D.get(0).size()).mapToObj(in -> "0").collect(Collectors.toCollection(ArrayList::new));
+            date.set(0, missingFossilDates[d]);
+            modifiedPerson2D.add(date);
+        }
+        modifiedPerson2D = modifiedPerson2D.stream().sorted(Comparator.comparing(d -> d.get(0))).collect(Collectors.toCollection(ArrayList::new)); // sorts the username and score to
+        modifiedPerson2D.add(0, modifiedPerson2D.remove(modifiedPerson2D.size() - 2));
+        modifiedPerson2D.add(0, modifiedPerson2D.remove(modifiedPerson2D.size() - 1));
+        modifiedPerson2D = transposeMatrix(modifiedPerson2D);
+        for (int i = 1; i < modifiedPerson2D.size(); i++) {
+            String[] person = modifiedPerson2D.get(i).toArray(new String[0]);
+            if (person[1].equals("0")){
+                continue;
+            }
+            ArrayList<String> resultPerson = new ArrayList<>();
+            for (int d = 0; d < person.length; d++) {
+                if (d == 1) {
+                    resultPerson.add("Fossils");
+                } else if (d > 2 && i > 0) {
+                    resultPerson.add(Integer.parseInt(resultPerson.get(d - 1)) + Integer.parseInt(person[d]) + "");
+                } else {
+                    resultPerson.add(person[d]);
+                }
+            }
+            matrix.add(resultPerson);
+        }
+        csv = PApplet.loadStrings(new File("data\\stars.csv"));
+        removeCommas(csv);
+        String[] starDates = Arrays.stream(csv[0].split(",")).skip(2).collect(Collectors.toList()).toArray(new String[0]);
+        String[] missingStarDates = getMissingDates(starDates[0], LocalDate.now().plusDays(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),starDates);
+        modifiedPerson2D = Arrays.stream(csv).map(s -> new ArrayList<>(Arrays.asList(s.split(",")))).collect(Collectors.toCollection(ArrayList::new));
+        modifiedPerson2D = transposeMatrix(modifiedPerson2D);
+        for (int d = 0; d < missingStarDates.length; d++){
+            ArrayList<String> date = IntStream.range(0, modifiedPerson2D.get(0).size()).mapToObj(in -> "0").collect(Collectors.toCollection(ArrayList::new));
+            date.set(0, missingStarDates[d]);
+            modifiedPerson2D.add(date);
+        }
+        modifiedPerson2D = modifiedPerson2D.stream().sorted(Comparator.comparing(d -> d.get(0))).collect(Collectors.toCollection(ArrayList::new)); // sorts the username and score to
+        modifiedPerson2D.add(0, modifiedPerson2D.remove(modifiedPerson2D.size() - 2));
+        modifiedPerson2D.add(0, modifiedPerson2D.remove(modifiedPerson2D.size() - 1));
+        modifiedPerson2D = transposeMatrix(modifiedPerson2D);
+        for (int i = 1; i < modifiedPerson2D.size(); i++) {
+            String[] person = modifiedPerson2D.get(i).toArray(new String[0]);
+            if (person[1].equals("0")){
+                continue;
+            }
+            ArrayList<String> resultPerson = new ArrayList<>();
+            for (int d = 0; d < person.length; d++) {
+                if (d == 1) {
+                    resultPerson.add("Stars");
+                } else if (d > 2 && i > 0) {
+                    resultPerson.add(Integer.parseInt(resultPerson.get(d - 1)) + Integer.parseInt(person[d]) + "");
+                } else {
+                    resultPerson.add(person[d]);
+                }
+            }
+            matrix.add(resultPerson);
+        }
+        csv = PApplet.loadStrings(new File("data\\rocks.csv"));
+        removeCommas(csv);
+        String[] rockDates = Arrays.stream(csv[0].split(",")).skip(2).collect(Collectors.toList()).toArray(new String[0]);
+        String[] missingRockDates = getMissingDates(rockDates[0], LocalDate.now().plusDays(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")), rockDates);
+        modifiedPerson2D = Arrays.stream(csv).map(s -> new ArrayList<>(Arrays.asList(s.split(",")))).collect(Collectors.toCollection(ArrayList::new));
+        modifiedPerson2D = transposeMatrix(modifiedPerson2D);
+        for (int d = 0; d < missingRockDates.length; d++){
+            ArrayList<String> date = IntStream.range(0, modifiedPerson2D.get(0).size()).mapToObj(in -> "0").collect(Collectors.toCollection(ArrayList::new));
+            date.set(0, missingRockDates[d]);
+            modifiedPerson2D.add(date);
+        }
+        modifiedPerson2D = modifiedPerson2D.stream().sorted(Comparator.comparing(d -> d.get(0))).collect(Collectors.toCollection(ArrayList::new)); // sorts the username and score to
+        modifiedPerson2D.add(0, modifiedPerson2D.remove(modifiedPerson2D.size() - 2));
+        modifiedPerson2D.add(0, modifiedPerson2D.remove(modifiedPerson2D.size() - 1));
+        modifiedPerson2D = transposeMatrix(modifiedPerson2D);
+        for (int i = 1; i < modifiedPerson2D.size(); i++) {
+            String[] person = modifiedPerson2D.get(i).toArray(new String[0]);
+            if (person[1].equals("0")){
+                continue;
+            }
+            ArrayList<String> resultPerson = new ArrayList<>();
+            for (int d = 0; d < person.length; d++) {
+                if (d == 1) {
+                    resultPerson.add("Rocks");
+                    for (int da = 0; da < 199; da++){
+                        resultPerson.add("0");
+                    }
+                } else if (d > 2 && i > 0) {
+                    resultPerson.add(Integer.parseInt(resultPerson.get(d + 198)) + Integer.parseInt(person[d]) + "");
+                } else {
+                    resultPerson.add(person[d]);
+                }
+            }
+            matrix.add(resultPerson);
+        }
+
+        while (matrix.get(matrix.size() - 1).get(0).equals("0")){
+            matrix.remove(matrix.size() - 1);
+        }
+        matrix = transposeMatrix(matrix);
+        for (int i = 1; i < matrix.get(0).size(); i++){
+            matrix.get(0).set(i, matrix.get(0).get(i).substring(0, matrix.get(0).get(i).length() - 5));
+        }
+
+        writeToCSV(matrix);
+
+    }
+
+    private static String convertToCSVRow(ArrayList<String> data){
+        return data.stream().collect(Collectors.joining(","));
+
+    }
+
+    private static void writeToCSV(ArrayList<ArrayList<String>> matrix){
+        File output = new File("data\\out.csv");
+        try (PrintWriter pw = new PrintWriter(output)){
+            matrix.stream().map(DataReader::convertToCSVRow).forEach(pw::println);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        font = loadFont("Constantia-96.vlw");
-        numberFont = loadFont("CambriaMath-96.vlw");
-        String[] textFile = loadStrings("data\\out.csv");
-        String[] parts = textFile[0].split(",");
-        PEOPLE_COUNT = parts.length - 1;
-        DAY_LEN = textFile.length - 2;
-        Person[] people = new Person[PEOPLE_COUNT];
-        for (int i = 0; i < PEOPLE_COUNT; i++){
-            people[i] = new Person(this, parts[i+1], textFile[1].split(",")[i+1], TOP_VISIBLE, DAY_LEN);
-        }
-        for (int d = 0; d < DAY_LEN; d++){
-            String[] dataParts = textFile[d+2].split(",");
-            for(int p = 0; p < PEOPLE_COUNT; p++){
-                float val = Float.parseFloat(dataParts[p+1]);
-                people[p].values[d] = Float.parseFloat(dataParts[p+1]);
+    }
+
+    private static ArrayList<ArrayList<String>> transposeMatrix(ArrayList<ArrayList<String>> matrix){
+        int length = matrix.get(0).size();
+        for (ArrayList<String> arr : matrix){
+            //System.out.println(arr);
+            if (length != arr.size()) {
+                /*
+                System.out.println(arr.get(arr.size() - 1));
+                System.out.println(arr);
+                System.out.println(arr.size());
+                */
+                return null;
             }
         }
-
-        ArrayList<Person> orni = new ArrayList<>();
-        ArrayList<Person> fossils = new ArrayList<>();
-        ArrayList<Person> stars = new ArrayList<>();
-        ArrayList<Person> rocks = new ArrayList<>();
-
-        for (int p = 0; p < people.length; p++){
-            Person pe = people[p];
-            if (pe.event.equals("Orni")){
-                orni.add(pe);
-            } else if (pe.event.equals("Fossils")){
-                fossils.add(pe);
-            } else if (pe.event.equals("Stars")) {
-                stars.add(pe);
-            } else if (pe.event.equals("Rocks")) {
-                rocks.add(pe);
+        ArrayList<ArrayList<String>> result = new ArrayList<>();
+        for (int c = 0; c < length; c++){
+            ArrayList<String> arr = new ArrayList<>();
+            for (int r = 0; r < matrix.size(); r++){
+                arr.add(matrix.get(r).get(c));
             }
+            result.add(arr);
+        }
+        return result;
+    }
+
+    private static String[] removeCommas(String[] matrix){
+        for (int r = 0; r < matrix.length; r++){
+            matrix[r] = removeCommasInName(matrix[r]);
+        }
+        return matrix;
+    }
+
+    private static String removeCommasInName(String str){
+        boolean inQuotes = false;
+
+        for (int i = 0; i < str.length(); i++){
+            if (str.substring(i, i+1).equals("\""))
+                inQuotes = !inQuotes;
+            else if (inQuotes && str.substring(i, i+1).equals(","))
+                str = str.substring(0, i) + str.substring(i-- + 1);
         }
 
-
-        corners[NORTHWEST] = new BarGraph(orni.toArray(new Person[0]), font, numberFont, this, 10, 560, 80, 480, START_DATE, FRAMES_PER_DAY, TOP_VISIBLE, false);
-        corners[NORTHEAST] = new BarGraph(fossils.toArray(new Person[0]), font, numberFont, this, 960, 1510, 80, 480, START_DATE, FRAMES_PER_DAY, TOP_VISIBLE, false);
-        corners[SOUTHWEST] = new BarGraph(stars.toArray(new Person[0]), font, numberFont, this, 10, 560, 600, 1000, START_DATE, FRAMES_PER_DAY, TOP_VISIBLE,false);
-        corners[SOUTHEAST] = new BarGraph(rocks.toArray(new Person[0]), font, numberFont, this, 960, 1510, 600, 1000, ROCKS_START_DATE, FRAMES_PER_DAY, TOP_VISIBLE,false);
-
-
-        for (BarGraph bg : corners){
-            if (bg != null)
-                bg.setSettings(false, false, false);
-        }
-
-        videoExport = new VideoExport(this, "outputtedVideo.mp4");
-        videoExport.startMovie();
-    }
-    public void draw(){
-        fill(0);
-        background(0);
-        textAlign(RIGHT);
-        fill(255);
-        for (BarGraph bg : corners){
-            bg.draw(START_DATE, frames);
-        }
-        textFont(font, 40);
-        text(daysToDate(frames/FRAMES_PER_DAY + START_DAY,true), width/2 - 110,height/2 - 10);
-        videoExport.saveFrame();
-        if(getDayFromFrameCount(frames + 1) >= DAY_LEN){
-            videoExport.endMovie();
-            exit();
-        }
-        frames++;
+        return str;
     }
 
-    float getDayFromFrameCount(int fc){
-        return fc/FRAMES_PER_DAY + START_DAY;
+    private static String[] getMissingDates(String startDate, String endDate, String[] dates){
+        return LocalDate.parse(startDate, DateTimeFormatter.ofPattern("yyyy-MM-dd")).datesUntil(LocalDate.parse(endDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"))).map(d -> d.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))).filter(d -> !Arrays.asList(dates).contains(d)).collect(Collectors.toList()).toArray(new String[0]);
     }
 
-    public void settings(){
-        size(1920, 1080);
-    }
 
-    int dateToDaysShort(String s){
-        return dateToDays(s) - START_DATE;
-    }
-
-    int dateToDays(String s){
-        int year = Integer.parseInt(s.substring(0,4))-1900;
-        int month = Integer.parseInt(s.substring(5,7))-1;
-        int date = Integer.parseInt(s.substring(8,10));
-        Date d1 = new Date(year, month, date, 6, 6, 6);
-        int days = (int)(d1.getTime()/86400000L);
-        return days;
-    }
-
-    String daysToDate(float daysF, boolean longForm){
-        int days = (int)daysF+START_DATE+1;
-        Date d1 = new Date();
-        d1.setTime(days* 86400000L);
-        int year = d1.getYear()+1900;
-        int month = d1.getMonth()+1;
-        int date = d1.getDate();
-        if(longForm){
-            return year+" "+monthNames[month-1]+" "+date;
-        }else{
-            return year+"-"+ PApplet.nf(month,2,0)+"-"+ PApplet.nf(date,2,0);
-        }
-    }
 }
